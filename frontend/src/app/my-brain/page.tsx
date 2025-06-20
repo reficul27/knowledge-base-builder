fb6b57a1a9332d468e51644be4dbc4fb8b9e7dd3
reficul27 <meisner.rene@proton.me>
2025-06-20 18:26:54 +0200

    Update page import to use new Knowledge Map component name

--- frontend/src/app/my-brain/page.tsx
+++ frontend/src/app/my-brain/page.tsx
@@ -1,6 +1,6 @@
-// src/app/my-brain/page.tsx - Neural Network Archive View
-import NeuralNetworkMindmap from '@/components/mindmap/NeuralNetworkMindmap'
+// src/app/my-brain/page.tsx - Knowledge Map Archive View
+import KnowledgeMapMindmap from '@/components/mindmap/NeuralNetworkMindmap'
 
-export default function MyBrainPage() {
-  return <NeuralNetworkMindmap />
+export default function KnowledgeMapPage() {
+  return <KnowledgeMapMindmap />
 }
